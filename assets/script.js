b158449f01d3009a0f18d8c19d531f183588a65a
SereneCai <73454440+SereneCai@users.noreply.github.com>
2020-12-09 14:09:03 +0800

    initial commit

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,20 @@
-//JS
+var myModal = document.getElementById("myModal");
+var clickLogin = document.getElementById("loginclick");
+var span = document.getElementsByClassName("closetag")[0];
+
+clickLogin.addEventListener('click', function(){
+  if (myModal.style.display == "none" || myModal.style.display ==''){
+    myModal.style.display = "block";
+  } else
+  myModal.style.display = "none";
+});
+
+span.addEventListener('click', function(){
+  if (myModal.style.display == "block")
+  myModal.style.display = "none";
+})
+
+window.addEventListener('click', function(event){
+  if (event.target == myModal)
+  {myModal.style.display = "none";}
+})
