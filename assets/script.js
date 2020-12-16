1299f6118f4578c38837d25f6c6dd8af5c1a0b83
ghsyafii <73457694+ghsyafii@users.noreply.github.com>
2020-12-16 20:01:13 +0800

    contact.html: added validation, fixed margins

--- assets/script.js
+++ assets/script.js
@@ -129,6 +129,19 @@ function validUser() {
 
 }
 
+
+
+//error messages for contact
+
+// const name = document.getElementById('name')
+// const email = document.getElementById('email')
+// const message = document.getElementById('message')
+// const form = document.getElementById('form')
+
+// form.addEventListener('submit', (e) => {
+//   e.preventDefault()
+// })
+
 //foreach method try later
 
 //Define a class called ModelController that contains a data structure to store the items added in furniture.json
