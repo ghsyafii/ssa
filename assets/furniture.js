ca363793e5d2c285c2f74afbd8297f26e281c8e5
ghsyafii <73457694+ghsyafii@users.noreply.github.com>
2020-12-17 21:19:56 +0800

    furniture.js: refactored

--- assets/furniture.js
+++ assets/furniture.js
@@ -1,18 +1,6 @@
 //Fetch JSON to HTML page
 //Insert JSON items to paragraph with #cards id
 
-// fetch('furniture.json')
-//     .then((response) => response.json())
-//     .then((json) => {
-//         console.log(json);
-//         const cards = json
-//         let output = "";
-//         for (let i = 0; i < json.length; i++) {
-//             output += "<p>" + json[i].name + ", " + json[i].price + "<p>";
-//             document.getElementById("cards").innerHTML = output;
-//         }
-//     })
-
 fetch('furniture.json')
     .then((response) => response.json())
     .then((json) => {
@@ -20,21 +8,16 @@ fetch('furniture.json')
         const cards = json
         let output = "";
         for (let i = 0; i < json.length; i++) {
-            output += 
-            '<img class="card-img-top animate__animated animate__fadeIn" src="'
-            +
-            json[i].image +
-            '" alt="Card image cap">\n' +
+            output +=
 
-            '<div class="card" style="width: 18rem;">\n' +
-        '    <div class="card-body">\n' +
-        '        <h5 class="card-title">' + json[i].name + '</h5>\n' +
-        '        <p class="card-text">' + json[i].price + '</p>\n'  +
-        '    </div>\n' +
-        '</div>\n' +
-        '<br/>';
+    `<div class="card p-1">
+        <img class="card-img-top animate__animated animate__fadeIn" src="${json[i].image}" alt="">
+    
+        <div class="card-body">
+            <h5 class="card-title">${json[i].name}</h5>
+            <p class="card-text">${json[i].price}</p>
+        </div>
+    </div>`
             document.getElementById("cards").innerHTML = output;
         }
     })
-
-
