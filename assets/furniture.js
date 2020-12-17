21858a4855c43081f9e46c2328a420a03e1c47cc
ghsyafii <73457694+ghsyafii@users.noreply.github.com>
2020-12-17 10:06:43 +0800

    furniture.json: added images

--- assets/furniture.js
+++ assets/furniture.js
@@ -1,6 +1,18 @@
 //Fetch JSON to HTML page
 //Insert JSON items to paragraph with #cards id
 
+// fetch('furniture.json')
+//     .then((response) => response.json())
+//     .then((json) => {
+//         console.log(json);
+//         const cards = json
+//         let output = "";
+//         for (let i = 0; i < json.length; i++) {
+//             output += "<p>" + json[i].name + ", " + json[i].price + "<p>";
+//             document.getElementById("cards").innerHTML = output;
+//         }
+//     })
+
 fetch('furniture.json')
     .then((response) => response.json())
     .then((json) => {
@@ -8,7 +20,19 @@ fetch('furniture.json')
         const cards = json
         let output = "";
         for (let i = 0; i < json.length; i++) {
-            output += "<p>" + json[i].name + ", " + json[i].price + "<p>";
+            output += 
+            '<img class="card-img-top animate__animated animate__fadeIn" src="'
+            +
+            json[i].image +
+            '" alt="Card image cap">\n' +
+
+            '<div class="card" style="width: 18rem;">\n' +
+        '    <div class="card-body">\n' +
+        '        <h5 class="card-title">' + json[i].name + '</h5>\n' +
+        '        <p class="card-text">' + json[i].price + '</p>\n'  +
+        '    </div>\n' +
+        '</div>\n' +
+        '<br/>';
             document.getElementById("cards").innerHTML = output;
         }
     })
