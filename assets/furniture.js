7916a35b1ab0e1ab64a8d5fe9e31c2f9a57046fd
ghsyafii <73457694+ghsyafii@users.noreply.github.com>
2020-12-17 09:29:21 +0800

    furniture.json: added price to innerHTML

--- assets/furniture.js
+++ assets/furniture.js
@@ -2,15 +2,15 @@
 //Insert JSON items to paragraph with #cards id
 
 fetch('furniture.json')
-            .then((response) => response.json())
-            .then((json) => {
-                console.log(json);
-                const cards = json
-                let output = "";
-                for(let i = 0; i < json.length; i++){
-                    output += "<p>" + json[i].name + "<p>";
-                    document.getElementById("cards").innerHTML = output;
-                }
-            })
+    .then((response) => response.json())
+    .then((json) => {
+        console.log(json);
+        const cards = json
+        let output = "";
+        for (let i = 0; i < json.length; i++) {
+            output += "<p>" + json[i].name + ", " + json[i].price + "<p>";
+            document.getElementById("cards").innerHTML = output;
+        }
+    })
 
 
