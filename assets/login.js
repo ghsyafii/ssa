7c2aed8170f0af17fc626149ce70393a1b366bb5
ghsyafii <73457694+ghsyafii@users.noreply.github.com>
2021-02-01 20:04:48 +0800

    fixed positioning of Admin button

--- assets/login.js
+++ assets/login.js
@@ -118,16 +118,3 @@ firebase.auth().onAuthStateChanged(firebaseUser => {
     }
 })
 
-// loginForm.onsubmit = () => {
-//
-// }
-
-//Initialise authentication
-// const auth = firebase.auth();
-//
-// auth.signInWithEmailAndPassword(email,pass);
-// auth.createUserWithEmailAndPassword(email,pass);
-// auth.onAuthStateChanged(firebaseUser => {
-//
-// })
-
