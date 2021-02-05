da90788bfad58d95a7f00a5953521e06320b7906
SereneCai <73454440+SereneCai@users.noreply.github.com>
2021-02-05 12:38:21 +0800

    added spring boot

---
+++ src/main/java/com/project/ssa1/Ssa1Application.java
@@ -0,0 +1,13 @@
+package com.project.ssa1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ssa1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ssa1Application.class, args);
+	}
+
+}
